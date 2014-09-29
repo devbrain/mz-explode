914689ea270303495892485b8f6c5ce57fadb3d2
igor.gutnik@gmail.com <igor.gutnik@gmail.com@45e577e9-b647-14d1-e141-875cfe7dd6db>
2014-09-29 14:55:05 +0000

    unittest colour output

--- trunk/src/unittest/main.cc
+++ trunk/src/unittest/main.cc
@@ -1,7 +1,265 @@
 #include <stddef.h>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <stdexcept>
+
+enum colour {
+	DARKBLUE = 1,
+	DARKGREEN,
+	DARKTEAL,
+	DARKRED,
+	DARKPINK,
+	DARKYELLOW,
+	GRAY,
+	DARKGRAY,
+	BLUE,
+	GREEN,
+	TEAL,
+	RED,
+	PINK,
+	YELLOW,
+	WHITE
+};
+
+#if defined(WIN32)
+#include <Windows.h>
+struct setcolour
+{
+	colour _c;
+	HANDLE _console_handle;
+
+
+	setcolour(colour c, HANDLE console_handle)
+		: _c(c), _console_handle(0)
+	{
+		_console_handle = console_handle;
+	}
+
+	setcolour(colour c)
+		: _c(c), _console_handle(GetStdHandle(STD_OUTPUT_HANDLE))
+	{
+
+	}
+	
+};
+
+std::basic_ostream <char>& operator << (std::basic_ostream<char> &s, const setcolour &ref)
+{
+	SetConsoleTextAttribute(ref._console_handle, ref._c);
+	return s;
+}
+
+#else
+#include <stdio.h>
+
+#define CC_CONSOLE_COLOR_DEFAULT "\033[0m"
+#define CC_FORECOLOR(C) "\033[" #C "m"
+#define CC_BACKCOLOR(C) "\033[" #C "m"
+#define CC_ATTR(A) "\033[" #A "m"
+
+namespace zkr
+{
+	class cc
+	{
+	public:
+
+		class fore
+		{
+		public:
+			static const char *black;
+			static const char *blue;
+			static const char *red;
+			static const char *magenta;
+			static const char *green;
+			static const char *cyan;
+			static const char *yellow;
+			static const char *white;
+			static const char *console;
+
+			static const char *lightblack;
+			static const char *lightblue;
+			static const char *lightred;
+			static const char *lightmagenta;
+			static const char *lightgreen;
+			static const char *lightcyan;
+			static const char *lightyellow;
+			static const char *lightwhite;
+		};
+
+		class back
+		{
+		public:
+			static const char *black;
+			static const char *blue;
+			static const char *red;
+			static const char *magenta;
+			static const char *green;
+			static const char *cyan;
+			static const char *yellow;
+			static const char *white;
+			static const char *console;
+
+			static const char *lightblack;
+			static const char *lightblue;
+			static const char *lightred;
+			static const char *lightmagenta;
+			static const char *lightgreen;
+			static const char *lightcyan;
+			static const char *lightyellow;
+			static const char *lightwhite;
+		};
+
+		static char *color(int attr, int fg, int bg);
+		static const char *console;
+		static const char *underline;
+		static const char *bold;
+	};
+}
+namespace zkr
+{
+	enum Color
+	{
+		Black,
+		Red,
+		Green,
+		Yellow,
+		Blue,
+		Magenta,
+		Cyan,
+		White,
+		Default = 9
+	};
+
+	enum Attributes
+	{
+		Reset,
+		Bright,
+		Dim,
+		Underline,
+		Blink,
+		Reverse,
+		Hidden
+	};
+
+	char *cc::color(int attr, int fg, int bg)
+	{
+		static char command[13];
+
+		/* Command is the control command to the terminal */
+		sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+		return command;
+	}
+
+
+	const char *cc::console = CC_CONSOLE_COLOR_DEFAULT;
+	const char *cc::underline = CC_ATTR(4);
+	const char *cc::bold = CC_ATTR(1);
+
+	const char *cc::fore::black = CC_FORECOLOR(30);
+	const char *cc::fore::blue = CC_FORECOLOR(34);
+	const char *cc::fore::red = CC_FORECOLOR(31);
+	const char *cc::fore::magenta = CC_FORECOLOR(35);
+	const char *cc::fore::green = CC_FORECOLOR(92);
+	const char *cc::fore::cyan = CC_FORECOLOR(36);
+	const char *cc::fore::yellow = CC_FORECOLOR(33);
+	const char *cc::fore::white = CC_FORECOLOR(37);
+	const char *cc::fore::console = CC_FORECOLOR(39);
+
+	const char *cc::fore::lightblack = CC_FORECOLOR(90);
+	const char *cc::fore::lightblue = CC_FORECOLOR(94);
+	const char *cc::fore::lightred = CC_FORECOLOR(91);
+	const char *cc::fore::lightmagenta = CC_FORECOLOR(95);
+	const char *cc::fore::lightgreen = CC_FORECOLOR(92);
+	const char *cc::fore::lightcyan = CC_FORECOLOR(96);
+	const char *cc::fore::lightyellow = CC_FORECOLOR(93);
+	const char *cc::fore::lightwhite = CC_FORECOLOR(97);
+
+	const char *cc::back::black = CC_BACKCOLOR(40);
+	const char *cc::back::blue = CC_BACKCOLOR(44);
+	const char *cc::back::red = CC_BACKCOLOR(41);
+	const char *cc::back::magenta = CC_BACKCOLOR(45);
+	const char *cc::back::green = CC_BACKCOLOR(42);
+	const char *cc::back::cyan = CC_BACKCOLOR(46);
+	const char *cc::back::yellow = CC_BACKCOLOR(43);
+	const char *cc::back::white = CC_BACKCOLOR(47);
+	const char *cc::back::console = CC_BACKCOLOR(49);
+
+	const char *cc::back::lightblack = CC_BACKCOLOR(100);
+	const char *cc::back::lightblue = CC_BACKCOLOR(104);
+	const char *cc::back::lightred = CC_BACKCOLOR(101);
+	const char *cc::back::lightmagenta = CC_BACKCOLOR(105);
+	const char *cc::back::lightgreen = CC_BACKCOLOR(102);
+	const char *cc::back::lightcyan = CC_BACKCOLOR(106);
+	const char *cc::back::lightyellow = CC_BACKCOLOR(103);
+	const char *cc::back::lightwhite = CC_BACKCOLOR(107);
+}
+
+struct setcolour
+{
+	const char* _code;
+	setcolour(colour c)
+		: _code (" ")
+	{
+		switch (c)
+		{
+		case DARKBLUE:
+			_code = zkr::cc::fore::blue;
+			break;
+		case DARKGREEN:
+			_code = zkr::cc::fore::green;
+			break;
+		case DARKTEAL:
+			_code = zkr::cc::fore::cyan;
+			break;
+		case DARKRED:
+			_code = zkr::cc::fore::red;
+			break;
+		case DARKPINK:
+			_code = zkr::cc::fore::magenta;
+			break;
+		case DARKYELLOW:
+			_code = zkr::cc::fore::yellow;
+			break;
+		case GRAY:
+			_code = zkr::cc::fore::lightwhite;
+			break;
+		case DARKGRAY:
+			_code = zkr::cc::fore::white;
+			break;
+		case BLUE:
+			_code = zkr::cc::fore::lightblue;
+			break;
+		case GREEN:
+			_code = zkr::cc::fore::lightgreen;
+			break;
+		case TEAL:
+			_code = zkr::cc::fore::lightcyan;
+			break;
+		case RED:
+			_code = zkr::cc::fore::lightred;
+			break;
+		case PINK:
+			_code = zkr::cc::fore::lightmagenta;
+			break;
+		case YELLOW:
+			_code = zkr::cc::fore::lightyellow;
+			break;
+		case WHITE:
+			_code = zkr::cc::fore::lightwhite;
+			break;
+		}
+	}
+};
+
+std::basic_ostream <char>& operator << (std::basic_ostream<char> &s, const setcolour &ref)
+{
+	s << ref._code;
+	return s;
+}
+
+
+#endif
 
 #include "explode/io.hh"
 #include "explode/exe_file.hh"
@@ -88,12 +346,15 @@ static void pklite_test(const char* test_name, const unsigned char* data, std::s
 	}
 	total_tests++;
 	const char* s_ok = "PASSED";
+	colour col = GREEN;
 	if (!ok)
 	{
 		failed_tests++;
 		s_ok = "FAILED";
+		col = RED;
 	}
-	std::cout << "TEST #" << total_tests << ": PKLITE-" << test_name << " " << s_ok << std::endl;
+	std::cout << setcolour(GRAY) << "TEST #" << total_tests << ": PKLITE-" << test_name << " "
+		<< setcolour(col) << s_ok << setcolour(GRAY) << std::endl;
 }
 
 #define CONCATENATE_DIRECT(s1,s2) s1##s2
@@ -107,10 +368,8 @@ static void pklite_test(const char* test_name, const unsigned char* data, std::s
 int main(int argc, char* argv[])
 {
 
-	PKLITE_TEST(E_112);
-
 	PKLITE_TEST(112);
-	
+	PKLITE_TEST(E_112);
 
 	PKLITE_TEST(115);
 	PKLITE_TEST(E_115);
