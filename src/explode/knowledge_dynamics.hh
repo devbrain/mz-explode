4cba6dd556f142949ddb9b1b9949a3eae4c43e34
igor.gutnik@gmail.com <igor.gutnik@gmail.com@45e577e9-b647-14d1-e141-875cfe7dd6db>
2014-10-19 15:42:18 +0000

    warnings fixed

--- src/explode/knowledge_dynamics.hh
+++ src/explode/knowledge_dynamics.hh
@@ -8,30 +8,26 @@
 
 namespace explode
 {
-	class input_exe_file;
-	class output_exe_file;
-	class input;
+  class input;
+  class knowledge_dynamics
+  {
+  public:
+    explicit knowledge_dynamics(input_exe_file& inp);
 
-	class knowledge_dynamics
-	{
-	public:
-		explicit knowledge_dynamics(input_exe_file& inp);
+    static bool accept(input_exe_file& inp);
 
-		static bool accept(input_exe_file& inp);
+    void unpack(output_exe_file& oexe);
+    uint32_t decomp_size() const;
+  private:
+    input&          m_file;
 
-		void unpack(output_exe_file& oexe);
-		uint32_t decomp_size() const;
-	private:
-		input&          m_file;
-		input_exe_file& m_exe_file;
-
-		uint32_t m_expected_size;
-		uint32_t m_code_offs;
+    uint32_t m_expected_size;
+    uint32_t m_code_offs;
 		
-		uint16_t m_header[exe_file::MAX_HEADER_VAL];
+    uint16_t m_header[exe_file::MAX_HEADER_VAL];
 
 
-	};
+  };
 } // ns explode
 
 #endif
